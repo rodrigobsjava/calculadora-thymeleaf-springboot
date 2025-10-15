5049cfc03acb66a4e7558b0be0bc3f78cff97d74
RodrigoBS56 <rodrigobarbosadesousa5608@gmail.com>
2025-10-15 18:07:52 -0400

    create project calculadora-thymeleaf-springboot

---
+++ src/main/java/com/rodrigobsjava/calculadora/calculadora_thymeleaf_springboot/CalculadoraThymeleafSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigobsjava.calculadora.calculadora_thymeleaf_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraThymeleafSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoraThymeleafSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rodrigobsjava/calculadora/calculadora_thymeleaf_springboot/CalculadoraThymeleafSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigobsjava.calculadora.calculadora_thymeleaf_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraThymeleafSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
